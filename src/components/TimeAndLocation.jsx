24a0a5b02901f7d57fb2e643e21a882df9a23aae
Alvin-Karanja <alvin2@proton.me>
2023-09-02 19:01:01 +0300

    create time and location components

---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function TimeAndLocation() {
+  return (
+    <div>
+      <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">
+          Saturday, 2 September 2023 | Local time: 5:15 PM
+        </p>
+      </div>
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">Nairobi, KE</p>
+      </div>
+    </div>
+  );
+}
+
+export default TimeAndLocation;
